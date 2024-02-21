9b2ca6dbdf9633ca55dffee8c35e772f2fd3c5af
sahil <sahilgupta1113@gmail.com>
2024-02-21 12:15:28 +0530

    update

---
+++ Q5.php
@@ -0,0 +1,10 @@
+<!-- Scenario 5: Checking for Discount Eligibility -->
+<?php
+$orderAmount = 1200; // Example order amount
+if ($orderAmount > 1000) {
+    echo "Congratulations! You qualify for a discount.";
+
+} else {
+    echo "You need to spend more to qualify for the discount.";
+}
+?>
