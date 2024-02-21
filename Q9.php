6021a12af4e418f31ef1e540f87475d19a6b7fd2
sahil <sahilgupta1113@gmail.com>
2024-02-21 12:27:44 +0530

    update

---
+++ Q9.php
@@ -0,0 +1,12 @@
+<?php
+
+$userAccountType = "premium"; // Example account type
+
+if ($userAccountType === "premium") {
+    echo "Welcome Premium User!";
+} else {
+    echo "Welcome Basic User!";
+
+}
+
+?>
