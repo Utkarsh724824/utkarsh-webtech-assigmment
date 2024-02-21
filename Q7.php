2714133b888a00b09df05e7c83eae48e9546d99c
sahil <sahilgupta1113@gmail.com>
2024-02-21 12:23:26 +0530

    update

---
+++ Q7.php
@@ -0,0 +1,11 @@
+<?php
+$current Temperature = 30; // Example temperature
+
+if ($currentTemperature > 25) {
+    echo "It's a sunny day!";
+} else {
+    echo "Consider taking an umbrella";
+
+}
+
+?>
