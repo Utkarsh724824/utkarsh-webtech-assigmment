8931cb8a02d002e14e48c9e95a02262ce5af59e0
sahil <sahilgupta1113@gmail.com>
2024-02-21 12:35:24 +0530

    update

---
+++ Q12.php
@@ -0,0 +1,9 @@
+<?php
+$userIsNew = true;
+
+if ($userIsNew) {
+    echo "Congratulations! You qualify for a trial period.";
+} else {
+    echo "Thank you for your interest in our service.";
+}
+?>
