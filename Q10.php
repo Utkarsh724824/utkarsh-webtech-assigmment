3ecc3bd1790f020321036215f693d5f8a1792182
sahil <sahilgupta1113@gmail.com>
2024-02-21 12:30:25 +0530

    update

---
+++ Q10.php
@@ -0,0 +1,10 @@
+<?php
+$articleTitle = "This is a very long article title exceeding 50 characters limit";
+if (strlen($articleTitle) > 50) {
+    $truncatedTitle = substr($articleTitle, 0, 50);
+    echo $truncatedTitle;
+} else {
+    echo $articleTitle;
+
+}
+?>
